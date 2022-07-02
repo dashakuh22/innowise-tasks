7d4c00f688d8fcf849c94fff417b0178a55fbb04
dashakuh22 <dashakuh14@gmail.com>
2022-07-02 12:36:07 +0300

    add solution for task-4

---
+++ Task4.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace src;
+
+use Exception;
+use InvalidArgumentException;
+
+class Task4
+{
+    public function main(string $input): string
+    {
+        try {
+            $words = preg_split('/[ _-]+/', $input);
+
+            for ($i = 0; $i < sizeof($words); $i++) {
+                $words[$i] = ucfirst($words[$i]);
+            }
+
+            return implode('', $words);
+
+        } catch (Exception $ex) {
+            throw new InvalidArgumentException();
+        }
+    }
+}
