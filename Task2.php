46197d299932f8c0d09ef5db728f476f9b3b3759
dashakuh22 <dashakuh14@gmail.com>
2022-07-02 11:48:09 +0300

    task-2 fixed

--- Task2.php
+++ Task2.php
@@ -2,34 +2,44 @@
 
 namespace src;
 
+use DateTime;
+use Exception;
+use InvalidArgumentException;
+
 class Task2
 {
-    public function updateDate($dateString)
+    /**
+     * @throws Exception
+     */
+    public function updateDate($dateString): DateTime
     {
-        $suppliedDate = new \DateTime($dateString);
-        $currentYear = (int)(new \DateTime())->format('Y');
-        return (new \DateTime())->setDate(
+        $suppliedDate = new DateTime($dateString);
+        $currentYear = (int)(new DateTime())->format('Y');
+        return (new DateTime())->setDate(
             $currentYear,
             (int)$suppliedDate->format('m'),
             (int)$suppliedDate->format('d')
         );
     }
 
-    public function main(string $date)
+    /**
+     * @throws Exception
+     */
+    public function main(string $date): string
     {
         $date_parts = explode('.', $date);
         if (!checkdate($date_parts[1], $date_parts[0], $date_parts[2])) {
-            throw new \InvalidArgumentException();
+            throw new InvalidArgumentException();
         }
 
         $date = $this->updateDate($date)->format('d.m.Y');
-        $inp = \DateTime::createFromFormat('d.m.Y', $date);
+        $inp = DateTime::createFromFormat('d.m.Y', $date);
 
-        $cur = \DateTime::createFromFormat('d.m.Y', date_create('now')->format('d.m.Y'));
+        $cur = DateTime::createFromFormat('d.m.Y', date_create()->format('d.m.Y'));
 
         if ($cur > $inp) {
             $date = $inp->modify('+1 year')->format('d.m.Y');
-            $inp = \DateTime::createFromFormat('d.m.Y', $date);
+            $inp = DateTime::createFromFormat('d.m.Y', $date);
         }
 
         return $cur->diff($inp)->format('%a');
