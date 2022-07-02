84e6f547c609e4933b944452d56d4011872445f5
dashakuh22 <dashakuh14@gmail.com>
2022-07-02 11:45:29 +0300

    task-1 fixed

--- Task1.php
+++ Task1.php
@@ -2,22 +2,26 @@
 
 namespace src;
 
-class Task1 {
+use Exception;
+use InvalidArgumentException;
 
-    public $result = [ 'More than 30', 'More than 20', 'More than 10', 'Equal or less than 10' ];
+class Task1
+{
+    public array $result = ['More than 30', 'More than 20', 'More than 10', 'Equal or less than 10'];
 
-    public function main(int $inputNumber) {
+    public function main(int $inputNumber)
+    {
         try {
-            $res = $inputNumber > 30 ?
-                $this->result[0] : ($inputNumber > 20 ?
-                    $this->result[1] : ($inputNumber > 10 ?
+            return $inputNumber > 30 ?
+                $this->result[0] : (
+                    $inputNumber > 20 ?
+                    $this->result[1] : (
+                        $inputNumber > 10 ?
                         $this->result[2] : $this->result[3]
                     )
                 );
-            return $res;
         } catch (Exception $ex) {
-            throw new \InvalidArgumentException();
+            throw new InvalidArgumentException();
         }
     }
-
 }
