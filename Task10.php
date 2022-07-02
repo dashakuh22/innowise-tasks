ee8da819d76bfacdd8b3b0f267088a3047cd0471
dashakuh22 <dashakuh14@gmail.com>
2022-07-02 14:10:49 +0300

    fix task-10

---
+++ Task10.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace src;
+
+use InvalidArgumentException;
+
+class Task10
+{
+    public function main(int $input): array
+    {
+        if ($input > 0) {
+            $arr = [$input];
+            while ($input != 1) {
+                if ($input % 2 == 0) {
+                    $input /= 2;
+                } else {
+                    $input *= 3;
+                    $input++;
+                }
+                $arr[] = $input;
+            }
+
+            return $arr;
+        } else {
+            throw new InvalidArgumentException();
+        }
+    }
+}
+
+$t = new Task10();
+echo '<pre>';
+print_r($t->main('12'));
+echo '</pre>';
