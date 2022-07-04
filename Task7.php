e71d4edb2290b3427a2535287228fa65759a7d31
dashakuh22 <dashakuh14@gmail.com>
2022-07-04 20:31:49 +0300

    fix task-7

--- Task7.php
+++ Task7.php
@@ -7,23 +7,20 @@ use InvalidArgumentException;
 
 class Task7
 {
-    public function main(array $arr, int $position)
+    public function main(array $arr, int $position): array
     {
         try {
             $arr_upd = [];
-            unset($arr[$position]);
+            print_r($arr);
+            unset($arr[$position - 1]);
 
             foreach ($arr as $el) {
                 $arr_upd[] = $el;
             }
-            echo '<pre>';
-            print_r($arr_upd);
-            echo '</pre>';
+
+            return $arr_upd;
         } catch (Exception $ex) {
             throw new InvalidArgumentException('Invalid arguments received. Check inputs. Must be array and int.');
         }
     }
 }
-
-//$t = new Task7();
-//$t->main( [1, 2, 3, 4, 5], 3);
