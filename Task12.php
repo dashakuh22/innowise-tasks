cc1b756be0c4ab03a3f7004ecd12f503847f1de3
dashakuh22 <dashakuh14@gmail.com>
2022-07-04 11:42:27 +0300

    add solution for task-12

--- Task12.php
+++ Task12.php
@@ -2,5 +2,50 @@
 
 namespace src;
 
-use Exception;
 use InvalidArgumentException;
+
+class Calculator
+{
+    protected float $lhs;
+    protected float $rhs;
+    protected float $res;
+
+    public function __construct(float $lhs, float $rhs)
+    {
+        $this->lhs = $lhs;
+        $this->rhs = $rhs;
+    }
+
+    public function getResult(): float
+    {
+        return $this->res;
+    }
+
+    public function add(): Calculator
+    {
+        $this->res = $this->lhs + $this->rhs;
+        return $this;
+    }
+
+    public function sub(): Calculator
+    {
+        $this->res = $this->lhs - $this->rhs;
+        return $this;
+    }
+
+    public function multiply(): Calculator
+    {
+        $this->res = $this->lhs * $this->rhs;
+        return $this;
+    }
+
+    public function divideBy($divider = 1): Calculator
+    {
+        if ($divider == 0) {
+            throw new InvalidArgumentException('Division by zero');
+        }
+        $this->res = $this->res / $divider;
+
+        return $this;
+    }
+}
