c70b6b469aaf6cd428712f1ff027511e7c5fb5f5
dashakuh22 <dashakuh14@gmail.com>
2022-07-06 16:02:50 +0300

    fix task-8

--- Task8.php
+++ Task8.php
@@ -2,14 +2,18 @@
 
 namespace src;
 
+use Exception;
 use InvalidArgumentException;
 
 class Task8
 {
-    public function print_recursive($arr, &$res)
+    public function print_recursive($arr, string &$res): string
     {
+        if (is_string($arr) || is_integer($arr)) {
+            return $res = $arr;
+        }
         foreach ($arr as $key => $val) {
-            if (gettype($val) == 'string') {
+            if (gettype($val) != 'object' && gettype($val) != 'array') {
                 $res .= "$key: $val <br>";
             } else {
                 $this->print_recursive($val, $res);
@@ -21,29 +25,37 @@ class Task8
 
     public function main(string $json): string
     {
-        $json_decoded = json_decode($json);
-        if (is_object($json_decoded) && json_last_error() === JSON_ERROR_NONE) {
+        try {
+            $json_decoded = json_decode($json);
+        } catch (Exception $exception) {
+            throw new InvalidArgumentException('Not json format');
+        }
+
+        if (json_last_error() === JSON_ERROR_NONE) {
             $result = '';
             $this->print_recursive($json_decoded, $result);
 
             return $result;
         } else {
-            throw new InvalidArgumentException('Invalid arguments provided. Check inputs. Must be a json string.');
+            throw new InvalidArgumentException('Bad input');
         }
     }
 }
-
-/*$str = '{
+/*
+$str = '{
 "Title": "The Cuckoos Calling",
 "Author": "Robert Galbraith",
 "Detail": {
-"Publisher": {
-"Last name": "Brown",
-"First name": "Little"
+"Publisher": "Little Brown"
 }
 }
-}
-';*/
-/*$str = '44';
-$t = new Task8();
-echo $t->main($str);*/
+';
+//$str = '[{"user_id":13,"username":"stack"},{"user_id":14,"username":"over"}]';
+//$str = '{"a": { "a.1":"Значение a1","a.2":"Значение a2"},"2":"Значение 2","3":"Значение 3","4":"Значение 4","5":"Значение 5"}';
+//$str = '{background-color:yellow;color:#000;padding:10px;width:650px;}';
+//$str = '[0,1,3]';
+//$str = '0123';
+ */
+//$t = new Task8();
+//$str = 123;
+//echo $t->main($str);
