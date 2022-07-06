a761318800da23bd53c471613cfdd8f25db71341
dashakuh22 <dashakuh14@gmail.com>
2022-07-06 21:14:35 +0300

    fix task-3

--- Task3.php
+++ Task3.php
@@ -2,34 +2,25 @@
 
 namespace src;
 
-use Exception;
 use InvalidArgumentException;
 
 class Task3
 {
     public static function main(int $number): int
     {
-        try {
-            $number_str = (string)abs($number);
-            while (strlen($number_str) > 1) {
-                $value = 0;
-                for ($i = 0; $i < strlen($number_str); $i++) {
-                    $value += (int)$number_str[$i];
-                }
-                $number_str = (string)$value;
-            }
-
-            return (int)$number_str;
-        } catch (Exception $exception) {
-            throw new InvalidArgumentException();
+        if (strlen(strval(abs($number))) <= 1) {
+            throw new InvalidArgumentException('Bad input');
         }
+
+        $number_str = (string)abs($number);
+        while (strlen($number_str) > 1) {
+            $value = 0;
+            for ($i = 0; $i < strlen($number_str); $i++) {
+                $value += (int)$number_str[$i];
+            }
+            $number_str = (string)$value;
+        }
+
+        return (int)$number_str;
     }
 }
-
-/*$t = new Task3();
-//echo $t->main('-111111111111m1111111');
-echo $t->main(1111111111111111111);
-echo '<br>';
-echo $t->main(-231.0);
-echo '<br>';
-echo $t->main('-231');*/
