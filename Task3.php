e14bf9851fa667a7d349e547defbdac707330be7
dashakuh22 <dashakuh14@gmail.com>
2022-07-06 10:20:17 +0300

    add some tests for task-3

--- Task3.php
+++ Task3.php
@@ -10,18 +10,33 @@ class Task3
     public function main(int $number): int
     {
         try {
-            $number_str = (string) $number;
-            while (strlen($number_str) > 1) {
-                $value = 0;
-                for ($i = 0; $i < strlen($number_str); $i++) {
-                    $value += $number_str[$i];
+            if (func_num_args() == 1 && is_integer($number)) {
+                $number_str = (string)$number;
+                while (strlen($number_str) > 1) {
+                    $value = 0;
+                    for ($i = 0; $i < strlen($number_str); $i++) {
+                        $value .= $number_str[$i];
+                    }
+                    $number_str = (string)$value;
                 }
-                $number_str = (string) $value;
-            }
 
-            return (int) $number_str;
+                return (int)$number_str;
+            } else {
+                throw new InvalidArgumentException('Bad input');
+            }
         } catch (Exception $ex) {
-            throw new InvalidArgumentException();
+            throw new InvalidArgumentException('Bad input');
         }
     }
 }
+
+$t = new Task3();
+//echo $t->main();
+echo '<br>';
+echo $t->main(111111111111111111111111111111111111111111111111111);
+echo '<br>';
+echo $t->main(231);
+echo '<br>';
+echo $t->main('231');
+echo '<br>';
+echo $t->main(-444);
