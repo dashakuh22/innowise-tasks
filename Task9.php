8bd68aefbf638671fb720b995253033e59640f59
dashakuh22 <dashakuh14@gmail.com>
2022-07-06 19:14:02 +0300

    fix task-9

--- Task9.php
+++ Task9.php
@@ -9,7 +9,7 @@ class Task9
     public function arrayHasOnlyInts(array $array): bool
     {
         foreach ($array as $value) {
-            if (!is_numeric($value)) {
+            if (!is_int($value) || $value <= 0) {
                 return false;
             }
         }
@@ -19,10 +19,10 @@ class Task9
 
     public function checkInput(array $arr, float|int $num): bool
     {
-        if (!is_array($arr) || sizeof($arr) < 3 || !$this->arrayHasOnlyInts($arr)) {
+        if (sizeof($arr) < 3 || !$this->arrayHasOnlyInts($arr)) {
             return false;
         }
-        if (!is_int($num) || $num < 0) {
+        if (!is_int($num) || $num <= 0) {
             return false;
         }
 
@@ -35,7 +35,7 @@ class Task9
             $res = [];
             for ($i = 0; $i < sizeof($arr) - 2; $i++) {
                 if ($arr[$i] + $arr[$i + 1] + $arr[$i + 2] === $number) {
-                    $res[] = $arr[$i].' + '.$arr[$i + 1].' + '.$arr[$i + 2].' = '.$number;
+                    $res[] = [$arr[$i].' + '.$arr[$i + 1].' + '.$arr[$i + 2].' = '.$number];
                 }
             }
 
@@ -45,8 +45,8 @@ class Task9
         }
     }
 }
-
-//$t = new Task9();
-//echo '<pre>';
-//print_r($t->main([0, '0', 0], 0));
-//echo '</pre>';
+/*
+$t = new Task9();
+echo '<pre>';
+print_r($t->main([2, 7, 7, 1, 8, 2, 7, 8, 7], 16));
+echo '</pre>';*/
