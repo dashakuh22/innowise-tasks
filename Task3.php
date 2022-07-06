3102e8f02e149fbf0c1053c5572ac27a98e78481
dashakuh22 <dashakuh14@gmail.com>
2022-07-06 11:36:24 +0300

    fix task-3

--- Task3.php
+++ Task3.php
@@ -7,36 +7,38 @@ use InvalidArgumentException;
 
 class Task3
 {
-    public function main(int $number): int
+
+    public static function main(int $number): int
     {
         try {
-            if (func_num_args() == 1 && is_integer($number)) {
-                $number_str = (string)$number;
+            if (func_num_args() == 1 && (is_numeric($number) || is_integer($number))) {
+                $number_str = (string)abs($number);
                 while (strlen($number_str) > 1) {
                     $value = 0;
                     for ($i = 0; $i < strlen($number_str); $i++) {
-                        $value .= $number_str[$i];
+                        $value += (int)$number_str[$i];
                     }
                     $number_str = (string)$value;
                 }
 
                 return (int)$number_str;
             } else {
-                throw new InvalidArgumentException('Bad input');
+                throw new InvalidArgumentException('Too many arguments or not integer input');
             }
-        } catch (Exception $ex) {
-            throw new InvalidArgumentException('Bad input');
+        } catch (Exception $exception) {
+            throw new InvalidArgumentException();
         }
     }
 }
 
-$t = new Task3();
+//$t = new Task3();
 //echo $t->main();
-echo '<br>';
-echo $t->main(111111111111111111111111111111111111111111111111111);
-echo '<br>';
-echo $t->main(231);
-echo '<br>';
-echo $t->main('231');
-echo '<br>';
-echo $t->main(-444);
+//echo '<br>';
+//echo $t->main('-111111111111m1111111');
+//echo $t->main(1111111111111111111);
+//echo '<br>';
+//echo $t->main(231);
+//echo '<br>';
+//echo $t->main('2h31');
+//echo '<br>';
+//echo $t->main(-444);
