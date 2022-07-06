5f22bc0f3f8aa17c5f43279631560a73bb098a7b
dashakuh22 <dashakuh14@gmail.com>
2022-07-06 20:36:32 +0300

    fix task-8

--- Task8.php
+++ Task8.php
@@ -25,13 +25,9 @@ class Task8
 
     public function main(string $json): string
     {
-        try {
-            $json_decoded = json_decode($json);
-        } catch (Exception $exception) {
-            throw new InvalidArgumentException('Not json format');
-        }
+        $json_decoded = json_decode($json);
 
-        if (json_last_error() === JSON_ERROR_NONE) {
+        if (is_object($json_decoded) && json_last_error() === JSON_ERROR_NONE) {
             $result = '';
             $this->print_recursive($json_decoded, $result);
 
@@ -41,25 +37,14 @@ class Task8
         }
     }
 }
-
-//$str = '[{"user_id":13,"username":"stack"},{"user_id":14,"username":"over"}]';
-//$str = '{"a": { "a.1":"Значение a1","a.2":"Значение a2"},"2":"Значение 2","3":"Значение 3","4":"Значение 4","5":"Значение 5"}';
-//$str = '{background-color:yellow;color:#000;padding:10px;width:650px;}';
-//$str = '[0,1,3]';
-//$str = '0123';
-/*$t = new Task8();
+/*
+$t = new Task8();
 $str = '{
 "Title": "The Cuckoos Calling",
 "Author": "Robert Galbraith",
 "Detail": {
-    "Publisher": {
-        "FI": {
-            "Who": {
-                "Pseudonym": "Little Brown"
-                }
-            }
-        }
-    }
+"Publisher": "Little Brown"
+}
 }
 ';
 echo $t->main($str);*/
