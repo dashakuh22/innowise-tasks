289c193975dd84d0a246cb6734968441e0408ebb
dashakuh22 <dashakuh14@gmail.com>
2022-07-06 20:42:07 +0300

    fix task-6

--- Task6.php
+++ Task6.php
@@ -6,17 +6,7 @@ use InvalidArgumentException;
 
 class Task6
 {
-    public string $START = '01.01.1900';
-    public string $END = '31.12.1999';
-
     public array $WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-    public function checkRange(string $date): bool
-    {
-        $date = strtotime($date);
-
-        return $date >= strtotime($this->START) && $date <= strtotime($this->END);
-    }
 
     public function dayCount(string $day, string $startDate, string $endDate, int $counter): int
     {
@@ -42,8 +32,7 @@ class Task6
         $lastDate = '01.' . $lastMonth . '.' . $lastYear;
 
         if (checkdate($month, '01', $year) && checkdate($lastMonth, '01', $lastYear)
-            && in_array($day, $this->WEEK_DAYS) && strtotime($date) < strtotime($lastDate)
-            && $this->checkRange($date) && $this->checkRange($lastDate)) {
+            && in_array($day, $this->WEEK_DAYS) && strtotime($date) < strtotime($lastDate)) {
             return $this->dayCount(
                 $day,
                 date_create($date)->format('d.m.Y'),
@@ -56,5 +45,5 @@ class Task6
     }
 }
 
-//$t = new Task6();
-//echo $t->main(1980, 1981, 2, 3, 'Sunday');
+/*$t = new Task6();
+echo $t->main(1980, 2021, 5, 10);*/
