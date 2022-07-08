f67a33dc8c673ce985e3727c0504091d3d65a236
dashakuh22 <dashakuh14@gmail.com>
2022-07-08 14:02:37 +0300

    last fix (pls) task-12

--- Task12.php
+++ Task12.php
@@ -5,86 +5,37 @@ namespace src;
 use Exception;
 use InvalidArgumentException;
 
-class Task12
+class AdditionalOperation
 {
-    protected float $lhs;
-    protected float $rhs;
     protected float $res;
 
-    public function __construct(float|int $lhs, float|int $rhs)
+    public function __construct(float|int $res)
     {
-        try {
-            $this->lhs = $lhs;
-            $this->rhs = $rhs;
-            $this->res = 0;
-        } catch (Exception $exception) {
-            throw new InvalidArgumentException('Bad parameters');
-        }
+        $this->res = $res;
     }
 
-    public function setRhs(float|int $rhs): void
-    {
-        $this->rhs = $rhs;
-    }
-
-    public function setLhs(float|int $lhs): void
-    {
-        $this->lhs = $lhs;
-    }
-
-    public function add(): Task12
-    {
-        $this->res = $this->lhs + $this->rhs;
-
-        return $this;
-    }
-
-    public function subtract(): Task12
-    {
-        $this->res = $this->lhs - $this->rhs;
-
-        return $this;
-    }
-
-    public function multiply(): Task12
-    {
-        $this->res = $this->lhs * $this->rhs;
-
-        return $this;
-    }
-
-    public function divide(): Task12
-    {
-        if ($this->rhs == 0) {
-            throw new InvalidArgumentException('Division by zero');
-        }
-        $this->res = $this->lhs / $this->rhs;
-
-        return $this;
-    }
-
-    public function addBy(float|int $value): Task12
+    public function addBy(float|int $value): AdditionalOperation
     {
         $this->res = $this->res + $value;
 
         return $this;
     }
 
-    public function subtractBy(float|int $value): Task12
+    public function subtractBy(float|int $value): AdditionalOperation
     {
         $this->res = $this->res - $value;
 
         return $this;
     }
 
-    public function multiplyBy(float|int $multiplier): Task12
+    public function multiplyBy(float|int $multiplier): AdditionalOperation
     {
         $this->res = $this->res * $multiplier;
 
         return $this;
     }
 
-    public function divideBy(float|int $divider): Task12
+    public function divideBy(float|int $divider): AdditionalOperation
     {
         if ($divider == 0) {
             throw new InvalidArgumentException('Division by zero');
@@ -100,7 +51,57 @@ class Task12
     }
 }
 
-//echo '<pre>';
-//$t = new Task12('1', '3');
-//echo $t->multiply()->divideBy();
-//echo '</pre>';
+class Task12
+{
+    protected float $lhs;
+    protected float $rhs;
+
+    public function __construct(float|int $lhs, float|int $rhs)
+    {
+        try {
+            $this->lhs = $lhs;
+            $this->rhs = $rhs;
+        } catch (Exception $exception) {
+            throw new InvalidArgumentException('Bad parameters');
+        }
+    }
+
+    public function setRhs(float|int $rhs): void
+    {
+        $this->rhs = $rhs;
+    }
+
+    public function setLhs(float|int $lhs): void
+    {
+        $this->lhs = $lhs;
+    }
+
+    public function add(): AdditionalOperation
+    {
+        return new AdditionalOperation($this->lhs + $this->rhs);
+    }
+
+    public function subtract(): AdditionalOperation
+    {
+        return new AdditionalOperation($this->lhs - $this->rhs);
+    }
+
+    public function multiply(): AdditionalOperation
+    {
+        return new AdditionalOperation($this->lhs * $this->rhs);
+    }
+
+    public function divide(): AdditionalOperation
+    {
+        if ($this->rhs == 0) {
+            throw new InvalidArgumentException('Division by zero');
+        }
+
+        return new AdditionalOperation($this->lhs / $this->rhs);
+    }
+}
+
+/*echo '<pre>';
+$t = new Task12('1', '0');
+echo $t->divide();
+echo '</pre>';*/
