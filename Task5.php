d3bc83083ab83d4f3756a3322688f8f261840bfe
dashakuh22 <dashakuh14@gmail.com>
2022-07-08 11:37:24 +0300

    last fix (I HOPE) task-5

--- Task5.php
+++ Task5.php
@@ -2,55 +2,61 @@
 
 namespace src;
 
-use GMP;
 use InvalidArgumentException;
 
 class Task5
 {
-    public function fib_by_len(int $n): GMP
+    public int $base = 10;
+
+    public function fib_by_len(int $n): string
     {
-        $F = [[gmp_init(1), gmp_init(1)], [gmp_init(1), gmp_init(0)]];
+        $F = [[0], [1]];
         $this->power($F, $n);
 
-        return $F[0][0];
+        $count = count($F) - 1;
+        $result = sprintf('%d', end($F[$count]));
+        for ($i = sizeof($F[$count]) - 2; $i >= 0; --$i) {
+            $result .= sprintf('%01d', $F[$count][$i]);
+        }
+
+        return $result;
     }
 
-    public function getLargeValue(GMP $num1, GMP $num2, GMP $num3, GMP $num4): GMP
+    public function getSum(array $arrA, array $arrB): array
     {
-        return gmp_add(gmp_mul($num1, $num2), gmp_mul($num3, $num4));
-    }
+        $carry = 0;
+        for ($i = 0; $i < max(sizeof($arrA), sizeof($arrB)) || $carry; ++$i) {
+            if ($i == sizeof($arrA)) {
+                $arrA[] = 0;
+            }
+            $arrA[$i] += $carry + ($i < sizeof($arrB) ? $arrB[$i] : 0);
+            $carry = $arrA[$i] >= $this->base;
+            if ($carry) {
+                $arrA[$i] -= $this->base;
+            }
+        }
 
-    public function multiply(array &$F, array $M): void
-    {
-        $x = $this->getLargeValue($F[0][0], $M[0][0], $F[0][1], $M[1][0]);
-        $y = $this->getLargeValue($F[0][0], $M[0][1], $F[0][1], $M[1][1]);
-        $z = $this->getLargeValue($F[1][0], $M[0][0], $F[1][1], $M[1][0]);
-        $w = $this->getLargeValue($F[1][0], $M[0][1], $F[1][1], $M[1][1]);
-
-        $F[0][0] = $x;
-        $F[0][1] = $y;
-        $F[1][0] = $z;
-        $F[1][1] = $w;
+        return $arrA;
     }
 
     public function power(array &$F, int $n): void
     {
-        $M = [[gmp_init(1), gmp_init(1)], [gmp_init(1), gmp_init(0)]];
-        do {
-            $this->multiply($F, $M);
-            $cur = gmp_strval($F[0][0]);
-        } while (strlen($cur) < $n);
+        $curLength = 0;
+        for ($i = 2; $curLength < $n; $i++) {
+            $F[$i] = $this->getSum($F[$i - 1], $F[$i - 2]);
+            $curLength = sizeof($F[$i]);
+        }
     }
 
-    public function main(int $n): GMP
+    public function main(int $n): string
     {
         if ($n > 0) {
-            return $n == 1 ? gmp_init(1) : $this->fib_by_len($n);
+            return $n == 1 ? 1 : $this->fib_by_len($n);
         } else {
             throw new InvalidArgumentException('Bad input');
         }
     }
 }
-
-/*$t = new Task5();
-echo $t->main(300);*/
+/*
+$t = new Task5();
+echo $t->main(4);*/
